9065513e74d1dfe5aba1a1c0ca043e85dc1caea8
ElioAucachi <130664573+ElioAucachi@users.noreply.github.com>
2024-10-01 09:55:32 -0300

    WEb_Api
    hola

---
+++ Usuario.cs
@@ -0,0 +1,13 @@
+
+namespace Api;
+
+public class Usuario{
+    public int IdUsuario {get; set;}
+    public required string Nombre {get; set;}
+    public required string Email {get; set;}
+    public required string usuario {get; set;}
+    public required string Contraseña {get; set;}
+    public bool Habilitado { get; set; } = true;
+    public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+}
---
+++ UsuarioRol.cs
@@ -0,0 +1,10 @@
+namespace Api;
+
+public class UsuarioRol{
+    public int IdUsuarioRol{get; set;}
+    public required string IdUsuario {get; set;}
+    public bool IdRol {get; set;} = true;
+ 
+
+    
+}
